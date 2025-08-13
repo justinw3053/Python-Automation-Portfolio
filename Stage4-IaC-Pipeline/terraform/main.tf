a5d3b5db185611e554ab955dfad1a2bb0cb0f1bb
justinw3053 <justinw3053@gmail.com>
2025-08-13 11:11:10 +0000

    Added IAM role to workflows

--- Stage4-IaC-Pipeline/terraform/main.tf
+++ Stage4-IaC-Pipeline/terraform/main.tf
@@ -1,3 +1,5 @@
+# main.tf
+
 # This block configures Terraform to use AWS as the cloud provider.
 provider "aws" {
   region = "eu-central-1" # Or your specified AWS region
@@ -8,6 +10,36 @@ resource "aws_ecs_cluster" "my_flask_app_cluster" {
   name = "my-flask-app-cluster"
 }
 
+# --- NEW: IAM Role for ECS Task Execution ---
+# This role allows ECS to perform actions on your behalf, such as pulling images from ECR.
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs-task-execution-role"
+
+  # The assume_role_policy specifies which entities can assume this role.
+  # Here, it allows the ECS tasks service to assume this role.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+# --- NEW: Attach managed policy to the IAM Role ---
+# This attaches a pre-defined AWS managed policy to our new IAM role.
+# AmazonECSTaskExecutionRolePolicy grants permissions necessary for ECS tasks,
+# including permission to pull images from ECR.
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy_attachment" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 # This resource defines the ECS Task Definition, which is a blueprint for our application.
 resource "aws_ecs_task_definition" "my_flask_app_task" {
   family = "my-flask-app-task"
@@ -16,6 +48,10 @@ resource "aws_ecs_task_definition" "my_flask_app_task" {
   network_mode             = "awsvpc"
   cpu                      = "256"
   memory                   = "512"
+
+  # --- MODIFIED: Add execution_role ---
+  # We now specify the ARN of the IAM role created above.
+  execution_role = aws_iam_role.ecs_task_execution_role.arn
 
   # This is a JSON string that defines the container itself.
   container_definitions = jsonencode([
